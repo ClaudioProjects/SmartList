9315281451f42f9a178b8c1df038cdddc91bd402
Claudio Junior Olimpio <claudiojr2002@hotmail.com>
2022-03-09 14:26:44 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,28 +24,3 @@ module.exports = {
     },
     devtool: 'source-map'
 };
-
-//  module.exports = {
-//     mode : 'development',
-//     entry : path.resolve(__dirname, 'frontend', 'stylesmain'),
-//     output : {
-//         path: path.resolve(__dirname, 'public', 'assets', 'js'),
-//          filename: 'styles.js'
-//      },
-//      module: {
-//          rules: [{
-//              exclude: /node_modules/,
-//              test: /\.js$/,
-//              use: {
-//                  loader: 'babel-loader',
-//                  options: {
-//                      presets: ['@babel/env']
-//                  }
-//              }
-//          }, {
-//              test: /\.css$/,
-//              use: ['style-loader', 'css-loader']
-//          }]
-//      },
-//      devtool: 'source-map'
-// };
